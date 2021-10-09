1ae845a6b1d0550d87bb973fd55b64e3f127984b
Ilya Merzlyakov <ilyamerzlyakov@gmail.com>
2021-10-09 19:08:30 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const test = async () => {
+    const headers = {
+      "Content-Type": "application/json;charset=utf-8",
+      // "Access-Control-Allow-Origin": "*",
+    };
+
+    console.log(`test`);
+    await fetch(`http://localhost:8080/deploydemo/api/v1/test-properties`, {
+      method: "POST",
+      headers: headers,
+      //   body: JSON.stringify({
+      //     nid: props.nid,
+      //     parent: props.messageId,
+      //     message: message,
+      //     userName: contextUser ? contextUser : `${userName} +  (гость)`,
+      //     root: props.root,
+      //     mainRoot: getMainRoot(),
+      //     topic: messageTopic,
+      //     captchaToken: captchaToken,
+      //   }),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log("data  " + data);
+      })
+      .catch((err) => {
+        console.log("err " + err);
+      });
+  };
+
+  //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
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TEST APP</h1>
+
+      <button onClick={test}> тест </button>
     </div>
   );
 }
