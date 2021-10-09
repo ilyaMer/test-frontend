0129e1d09209431e6624b89a4d015c0fcbef48d1
Ilya Merzlyakov <ilyamerzlyakov@gmail.com>
2021-10-09 20:10:38 +0300

    cors+++++++

--- src/App.js
+++ src/App.js
@@ -1,39 +1,48 @@
 import "./App.css";
+import axios from "axios";
 
 function App() {
   const test = async () => {
-    const headers = {
-      "Content-Type": "application/json;charset=utf-8",
-      "Access-Control-Allow-Origin": "http://localhost:4000",
-    };
-
-    console.log(`test+++++`);
-
-    await fetch(`http://localhost:8080/deploydemo/api/v1/test-properties`, {
-      mode: "cors",
-      method: "POST",
-      headers: headers,
-      credentials: "include",
-      //   body: JSON.stringify({
-      //     nid: props.nid,
-      //     parent: props.messageId,
-      //     message: message,
-      //     userName: contextUser ? contextUser : `${userName} +  (гость)`,
-      //     root: props.root,
-      //     mainRoot: getMainRoot(),
-      //     topic: messageTopic,
-      //     captchaToken: captchaToken,
-      //   }),
-    })
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        console.log("data  " + data);
+    axios
+      .post(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
       })
       .catch((err) => {
         console.log("err " + err);
       });
+
+    // const headers = {
+    //   "Content-Type": "application/json;charset=utf-8",
+    //   "Access-Control-Allow-Origin": "http://localhost:4000",
+    // };
+    // console.log(`test+++++`);
+    // await fetch(`http://localhost:8080/deploydemo/api/v1/test-properties`, {
+    //   mode: "cors",
+    //   method: "POST",
+    //   headers: headers,
+    //   credentials: "include",
+    //   //   body: JSON.stringify({
+    //   //     nid: props.nid,
+    //   //     parent: props.messageId,
+    //   //     message: message,
+    //   //     userName: contextUser ? contextUser : `${userName} +  (гость)`,
+    //   //     root: props.root,
+    //   //     mainRoot: getMainRoot(),
+    //   //     topic: messageTopic,
+    //   //     captchaToken: captchaToken,
+    //   //   }),
+    // })
+    //   .then(function (response) {
+    //     return response.json();
+    //   })
+    //   .then(function (data) {
+    //     console.log("data  " + data);
+    //   })
+    //   .catch((err) => {
+    //     console.log("err " + err);
+    //   });
   };
 
   //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
